567f71527862fb5d39c7f0e4521cc9f4474a21f3
shabnamsoni205 <89511255+shabnamsoni205@users.noreply.github.com>
2021-08-25 14:05:00 +0530

    Delete BootJPAProj-DoctorRecords directory

--- BootJPAProj-DoctorRecords/src/main/java/com/nt/repo/IPatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nt.entity.Patient;
-
-public interface IPatientRepo extends CrudRepository<Patient, Integer> {
-
-}
